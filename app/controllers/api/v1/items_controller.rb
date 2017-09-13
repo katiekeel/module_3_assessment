92b836503d8ccbefd9cfa28c5490a7bc2fb60d58
Katie Keel <akatiekeel@gmail.com>
2017-09-13 09:51:22 -0600

    Delete item through API passes

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,11 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: Item.find(params[:id])
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    head 204
+  end
+
 end
--- spec/requests/api/v1/user_deletes_an_item_spec.rb
+++ spec/requests/api/v1/user_deletes_an_item_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "User deletes an item through API and" do
   scenario "receives a 204 response if the record is deleted" do
+    items = create_list(:item, 5)
     # When I send a DELETE request to `/api/v1/items/1`
-
+    delete '/api/v1/items/1'
     # I receive a 204 JSON response if the record is successfully deleted
+    expect(status).to eq 204
+    expect(Item.count).to eq 4
   end
 end
